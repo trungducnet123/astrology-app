938a5b890568950c6a91cea24077023bea9622e1
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2023-10-25 17:59:10 +0700

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,38 +6,33 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer>
-      <div className="footer__download">
-        <img src={downloadApple} alt="Download on the App Store" />
-        <img src={downloadAndroid} alt="Download on Google Play" />
-      </div>
+      #<div className="footer__download">
+        #<img src={downloadApple} alt="Download on the App Store" />
+        #<img src={downloadAndroid} alt="Download on Google Play" />
+      #</div>
       <p className="footer__contact">
         Got any feedback? Up for a chat? Shoot me an email or direct message the
         socials below.
       </p>
       <ul className="footer__socials">
         <li>
-          <a href="https://github.com/dxenia">
-            <i className="fa-brands fa-github"></i>
+          <a href="https://kabastro.com">
+            <i className="fa-solid fa-house"></i>
           </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/dana-xenia-marasca/">
-            <i className="fa-brands fa-linkedin"></i>
+          <a href="https://kabala.vn/tools">
+            <i className="fa-solid fa-bars"></i>
           </a>
         </li>
         <li>
-          <a href="https://www.freecodecamp.org/dxenia">
-            <i className="fa-brands fa-free-code-camp"></i>
-          </a>
-        </li>
-        <li>
-          <a href="mailto:marascadanaxenia@gmail.com">
+          <a href="mailto:contact@kabastro.com">
             <i className="fa-solid fa-envelope"></i>
           </a>
         </li>
       </ul>
       <div className="footer__copyright">
-        <p>&copy; 2023 Lunar. All Rights Reserved.</p>
+        <p>&copy; 2023 Kabastro. All Rights Reserved.</p>
       </div>
     </footer>
   );

9bf379926a7b20aeff5dde1d3249944d335a7d89
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2023-10-25 18:20:14 +0700

    Update SubscribeForm.tsx

--- src/components/SubscribeForm/SubscribeForm.tsx
+++ src/components/SubscribeForm/SubscribeForm.tsx
@@ -1,43 +1,54 @@
-import React, { Component } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
-class KabalaForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      day: '',
-      month: '',
-      year: '',
-    };
-  }
+import Button from '../Button/Button';
 
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
+import './SubscribeForm.css';
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { name, day, month, year } = this.state;
-    // Thêm xử lý submit tại đây (ví dụ: gửi dữ liệu lên server)
-  };
-
-  render() {
-    const { name, day, month, year } = this.state;
-    return (
-        <form method="GET" action="https://number.kabala.vn/" onSubmit={this.handleSubmit}>
-          <div className="form-row">
-            <label htmlFor="name"><strong>Họ Tên:</strong></label>
-            <input type="text" placeholder="Họ tên của bạn" name="name" id="name" value={name} onChange={this.handleInputChange} required />
-            <label htmlFor="name"><strong>Ngày sinh:</strong></label>
-            <input type="number" placeholder="Ngày" name="day" id="day" value={day} onChange={this.handleInputChange} min="1" max="31" required />
-            <input type="number" placeholder="Tháng" name="month" id="month" value={month} onChange={this.handleInputChange} min="1" max="12" required />
-            <input type="number" placeholder="Năm" name="year" id="year" value={year} onChange={this.handleInputChange} min="1900" max="2099" required />
-            <input type="submit" className="button tiny success" value="Xem ngay" />
-          </div>
-        </form>
-    );
-  }
+interface FormProps {
+  onSubmit: (data: FormData) => void;
 }
 
-export default KabalaForm;
+export interface FormData {
+  name: string; // Đổi thành "name" thay vì "firstName"
+  day: string;
+  month: string;
+  year: string;
+}
+
+export const SubscribeForm = ({ onSubmit }: FormProps) => {
+  const initialFormData: FormData = {
+    name: '', // Đổi thành "name" thay vì "firstName"
+    day: '',
+    month: '',
+    year: '',
+  };
+
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit(formData);
+    setFormData(initialFormData);
+  };
+
+  const { name, day, month, year } = formData;
+
+  return (
+    <form method="GET" action="https://number.kabala.vn/" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <label htmlFor="name"><strong>Name:</strong></label>
+        <input type="text" placeholder="Họ tên của bạn" name="name" id="name" value={name} onChange={handleInputChange} required />
+        <label htmlFor="name"><strong>Ngày sinh:</strong></label>
+        <input type="number" placeholder="Ngày" name="day" id="day" value={day} onChange={handleInputChange} min="1" max="31" required />
+        <input type="number" placeholder="Tháng" name="month" id="month" value={month} onChange={handleInputChange} min="1" max="12" required />
+        <input type="number" placeholder="Năm" name="year" id="year" value={year} onChange={handleInputChange} min="1900" max="2099" required />
+        <input type="submit" className="button tiny success" value="Xem ngay" />
+      </div>
+    </form>
+  );
+};
